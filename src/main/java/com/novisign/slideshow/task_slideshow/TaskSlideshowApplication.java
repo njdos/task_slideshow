467fc65bc51159a0fbbef64835bb6b4e2f9b46b9
njdos <14njdos@gmail.com>
2024-12-11 23:10:15 +0200

    init

---
+++ src/main/java/com/novisign/slideshow/task_slideshow/TaskSlideshowApplication.java
@@ -0,0 +1,13 @@
+package com.novisign.slideshow.task_slideshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSlideshowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskSlideshowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/novisign/slideshow/task_slideshow/TaskSlideshowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novisign.slideshow.task_slideshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSlideshowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
